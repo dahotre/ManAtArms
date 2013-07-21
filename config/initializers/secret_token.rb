783ca0167afb99cfbab6efdc79ceef9b33b6d26a
Aniket Dahotre <dahotre.aniket@gmail.com>
2013-07-21 15:09:45 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManAtArms::Application.config.secret_token = '12c1a33913ee259bb72e1feab7b01e50307cc9be3583ebd993da9ad31d33c36c8000e036dea2cb6c9fb3c8c611a2093ff1b86ab89f9d65b74917b2cdff20a5b6'
